487f8cb5835f5688a0ba86bb15069684a7f2c370
Guilherme Pacheco <guilherme-p-medeiros@outlook.com>
2024-01-31 22:03:56 -0300

    edit quizz-component

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizzJson from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzComponent implements OnInit {
 
-  constructor() { }
+  protected title :string = "";
+  protected question :any[] = [];
+  
+  protected currentQuestion :any = [];
+  protected currentId :number = 0;
+
+  protected isFinish :boolean = false;
+
+  protected results :string[] = [];
+  protected resultFinish :string = "";
+
+  constructor() {
+  
+  }
 
   ngOnInit(): void {
+    this.title = quizzJson.title;
+    this.question  = quizzJson.questions
+
+    this.currentQuestion = this.question[0];
+    this.currentId = this.question[0].id;
+
+  }
+
+  protected restart(){
+    this.currentQuestion = this.question[0];
+    this.currentId = this.question[0].id;
+
+    this.results = [];
+    this.resultFinish = "";
+
+    this.isFinish = false;
+  }
+
+  protected nextStap(response:string){
+    this.results.push(response);
+    this.currentId += 1;
+
+    if(this.currentId < this.question.length){
+      this.currentQuestion = this.question[this.currentId];
+    } else {
+      this.isFinish = true;
+      this.response();
+    }
+  }
+
+  protected response(){ 
+    const countA :number= this.results.filter(item => item === 'A').length;
+    const countB :number= this.results.filter(item => item === 'B').length;
+
+    if(countA > countB){
+      this.resultFinish = quizzJson.results.A;
+    } else {
+      this.resultFinish = quizzJson.results.B;
+    }
+
   }
 
 }
